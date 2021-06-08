51f5490218a32a8db731323951446d072919733e
irkhamdayat <irkhammuh11@gmail.com>
2021-06-08 21:35:28 +0700

    Pertemuan 12
    Pertemuan 12

---
+++ Pertemuan 12/DUTA CODING/src/mvc/Model/ModelUser.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvc.Model;
+
+/**
+ *
+ * @author user
+ */
+public class ModelUser {
+    private String nik;
+    private String nama;
+    private String jenis_kelamin;
+    private String alamat;
+    private Integer usia;
+    private String alasan;
+    
+    public String getNik() {
+        return nik;
+    }
+
+    public void setNik(String nik) {
+        this.nik = nik;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJk() {
+        return jenis_kelamin;
+    }
+
+    public void setJk(String jenis_kelamin) {
+        this.jenis_kelamin = jenis_kelamin;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+    
+    public Integer getUsia() {
+        return usia;
+    }
+
+    public void setUsia(Integer usia) {
+        this.usia = usia;
+    }
+
+    public String getAlasan() {
+        return alasan;
+    }
+
+    public void setAlasan(String alasan) {
+        this.alasan = alasan;
+    }
+
+}
